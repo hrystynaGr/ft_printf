27f57198a6c60f082f0c909a260b3a3e02256455
hrystynaGr <khrystyna.grebenyuk@scoutgaminggroup.com>
2023-03-19 17:44:14 +0100

    Todo: 1. Add libft 2. Use itoa for 'd' and 'i' 3. How to compile with main()

--- ft_printf.h
+++ ft_printf.h
@@ -10,8 +10,6 @@
 int		ft_printf(const char *string, ...);
 int		ft_print_char(int c);
 int		ft_print_str(char *str);
-int		len_ptr(uintptr_t nb);
-void	ft_put_ptr(uintptr_t nb);
 int		ft_print_ptr(void *ptr);
 int		ft_print_hexa(unsigned long num, char type);
 int		ft_putnbr(int x);
